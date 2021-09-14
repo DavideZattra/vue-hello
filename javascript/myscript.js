acc03af529c1e7c0693431756c165cb5d78d4b7b
DavideZattra <d.zattra@gmail.com>
2021-09-14 15:41:45 +0200

    added h1 and img from data

---
+++ javascript/myscript.js
@@ -0,0 +1,12 @@
+var app = new Vue(
+	{
+		el : '#app',
+		data : {
+			message : 'I am a cat!',
+			msClass: 'text-center',
+			msClassSecond: 'fw-bold',
+			img: 'img/cat.jpg',
+			imgPlaceholder: 'cat image'
+		}
+	}
+);
